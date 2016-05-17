29c107a66d7b909d7af4995a18ced691262716c1
Kerdefi <nico.ncpc@free.fr>
2016-05-17 23:22:24 +0200

    encophysstart

--- src/encophys/encophys.js
+++ src/encophys/encophys.js
@@ -1,30 +1,60 @@
-var encophys = encophys||{};
+var encophys = encophys || {};
 
 encophys.world = function () {
+    "use strict";
     //Default timestep set to 50 frame per second
-    this.timestep = 20 ;
+    this.timestep = 20;
+    this.points = [];
+    this.forcesgravitydir = [];
 
     //Callback to load json config file
     this.loadJson = function (error, data) {
-        this.timestep = data["timestep"];
+        this.timestep = data.timestep;
+        this.materials = data.materials;
     }.bind(this);
 
-    //
+    //configpath = string
     this.init = function (configpath) {
         cc.loader.loadJson(configpath, this.loadJson);
+    };
+
+    this.addPoint = function (point) {
+        this.points.push (point);
+    };
+
+    this.addForcesgravitydir = function (force) {
+        this.forcesgravitydir.push (force);
+    };
+
+    this.removePoint = function (point) {
+        this.points.slice (this.points.indexOf(point),1);
+    };
+
+    this.removeForcesgravitydir = function (point) {
+        this.forcesgravitydir.slice (this.forcesgravitydir.indexOf(point),1);
     };
 
     this.update = function () {
     };
 };
 
-encophys.point = function () {
+//material = string ; mass = number ; position = Vec2 ; heat = number
+encophys.point = function (material, mass, position, heat) {
+    "use strict";
 
-    this.init = function (material, mass, heat) {
-        this.material=material;
-        this.position=2;
-        this.mass=mass;
-        this.heat=heat;
-        this.energy=0;
-    };
+    this.material = material;
+    this.position = position;
+    this.mass = mass;
+    this.heat = heat;
+    this.energy = cc.math.Vec2(0, 0);
+    this.speed = cc.math.Vec2(0, 0);
+};
+
+//type : gravitydir, gravitypoint, friction
+//g = number ; dir = Vec2
+encophys.forcegravitydir = function (g, dir) {
+    "use strict";
+
+    this.g = g;
+    this.dir = dir;
 };
